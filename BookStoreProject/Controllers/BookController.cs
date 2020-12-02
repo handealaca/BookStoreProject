2bde4165cfddd38e22d9f4ff7afe4e06ed9517bf
eceerden <zgeece.erden@gmail.com>
2020-12-02 22:58:27 +0300

    book section added

---
+++ BookStoreProject/Controllers/BookController.cs
@@ -0,0 +1,35 @@
+﻿using BookStoreProject.Models.ORM.Context;
+using BookStoreProject.Models.VM;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreProject.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookContext _bookcontext;
+        public BookController(BookContext bookContext)
+        {
+            _bookcontext = bookContext;
+        }
+        public IActionResult Index()
+        {
+            List<BookVM> books = _bookcontext.Books.Where(q => q.IsDeleted == false).Select(w => new BookVM()
+            {
+                BookID = w.ID,
+                Name  = w.Name,
+                Publisher = w.Publisher,
+                PublishDate = w.PublishDate,
+                AddDate = w.AddDate,
+                Edition = w.Edition,
+                IsDeleted = w.IsDeleted
+               
+            }).ToList();
+            return View(books);
+        }
+        //public IActionResult Add()
+    }
+}
---
+++ BookStoreProject/Models/VM/BookVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreProject.Models.VM
+{
+    public class BookVM
+    {
+        public int BookID { get; set; }
+
+        public string Name { get; set; }
+        public string Publisher { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string Edition { get; set; }
+
+        public DateTime? AddDate { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+
+
+    }
+}
