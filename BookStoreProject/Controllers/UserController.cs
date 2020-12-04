4ce719cf50ec97e61fa4c9282e5c2d958f52a4b6
Hande Alaca <69791848+handealaca@users.noreply.github.com>
2020-12-04 23:50:39 +0300

    User CRUD_List added

---
+++ BookStoreProject/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using BookStoreProject.Models.ORM.Context;
+using BookStoreProject.Models.VM;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreProject.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly BookContext _bookcontext;
+        public UserController(BookContext bookContext)
+        {
+            _bookcontext = bookContext;
+        }
+        public IActionResult Index()
+        {
+            List<UserVM> users = _bookcontext.Users.Where(q => q.IsDeleted == false).Select(q => new UserVM()
+            {
+                UserID = q.ID,
+                Name = q.Name,
+                SurName = q.SurName,
+                Email = q.Email,
+                Password=q.Password,
+                Comments=q.Comments,
+                AddDate=q.AddDate,
+                IsDeleted = q.IsDeleted,
+                BirthDate = q.BirthDate
+            }).ToList();
+
+            return View(users);
+        }
+    }
+}
